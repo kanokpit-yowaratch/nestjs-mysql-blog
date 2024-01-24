f102173fc26770369b24f8346fc6e0fed55d6d9a
Kanokpit Yowaratch <kanokpit.y@gmail.com>
2024-01-24 19:46:43 +0700

    Enhance Swagger docs

--- src/blogs/dto/create-blog.dto.ts
+++ src/blogs/dto/create-blog.dto.ts
@@ -1,13 +1,27 @@
+import { ApiProperty } from '@nestjs/swagger';
+
 export class CreateBlogDto {
   category_id: number;
+
+  @ApiProperty({
+    example: 'Swimming at the Sea',
+    required: true
+ })
   title: string;
+
   slug: string;
   description: string;
   details: string; // text
   seo_title: string;
   seo_keywords: string;
   seo_description: string;
+  
+  @ApiProperty({
+    example: 'cover-blog-file-name.jpg',
+    required: false
+ })
   cover_path: string;
+
   count_view: number;
   related: string;
   // created_at: string;
