6f4602e043f48f9d8776afbe55bf5c1ea723e292
Kanokpit Yowaratch <kanokpit.y@gmail.com>
2024-02-10 00:23:21 +0700

    Post landing page with pagination

--- src/blogs/post.service.ts
+++ src/blogs/post.service.ts
@@ -1,7 +1,8 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Blog } from './entities/blog.entity';
-import { Repository } from 'typeorm';
+import { Brackets, Repository } from 'typeorm';
+import { Pagination, PostWithPagination } from './page.interface';
 
 @Injectable()
 export class PostService {
@@ -9,6 +10,34 @@ export class PostService {
     @InjectRepository(Blog)
     private blogsRepository: Repository<Blog>,
   ) { }
+
+  async findAllActive(page: Pagination, keyword: string): Promise<PostWithPagination> {
+    const take = page.limit || 10
+    const skip = page.offset || 0
+
+    const result = this.blogsRepository.createQueryBuilder('blog')
+      .where("blog.active_status = :active_status", { active_status: 0 });
+
+    if (keyword) {
+      result
+        .andWhere(
+          new Brackets((qb) => {
+            qb.where("blog.title LIKE :keyword", { keyword: `%${keyword}%` })
+              .orWhere("blog.description LIKE :keyword", { keyword: `%${keyword}%` })
+          }),
+        );
+    }
+
+    const tempResult = await result.take(take).skip(skip).getMany();
+
+    return {
+      data: tempResult,
+      count: tempResult.length,
+      page: (page.page + 1),
+      limit: page.limit,
+      offset: page.offset
+    }
+  }
 
   async detail(slug: string): Promise<Blog | null> {
     const post = await this.blogsRepository.findOne({ where: { slug: slug } });
